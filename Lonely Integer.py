21d9ee9bcd2d7ec0a7cc8bca621d5fd482f8d1f8
unknown <mmohamedabdelm@gmail.com>
2023-12-19 09:08:33 +0200

    Lonely Integer

---
+++ Lonely Integer.py
@@ -0,0 +1,25 @@
+def lonelyinteger(arr):
+    result = 0
+    for num in arr:
+        result ^= num  # استخدام XOR لإلغاء تأثير الأرقام المتكررة
+
+    return result
+
+
+# استدعاء الدالة وطباعة النتيجة
+print(lonelyinteger([3,5,2,3,5]))
+
+
+
+
+# def lonelyinteger(arr):
+#     # حساب مجموع العناصر المتكررة (مضاعفة)
+#     double_sum = sum(set(arr)) * 2
+    
+#     # حساب مجموع الكل
+#     total_sum = sum(arr)
+    
+#     # الفارق بين مجموع الكل ومجموع العناصر المتكررة يعطي الرقم الفريد
+#     unique_number = total_sum - double_sum
+    
+#     return unique_number
--- Time Conversion.py
+++ Time Conversion.py
@@ -1,19 +1,37 @@
 
-def convert(string):
+# def convert(string):
 
-      if string[-2:] == "AM" and string[:2] == "12":
-         return "00" + string[2:-2]
+#       if string[-2:] == "AM" and string[:2] == "12":
+#          return "00" + string[2:-2]
 
-      elif string[-2:] == "AM":
-         return string[:-2]
+#       elif string[-2:] == "AM":
+#          return string[:-2]
 
-      elif string[-2:] == "PM" and string[:2] == "12":
-         return string[:-2]
+#       elif string[-2:] == "PM" and string[:2] == "12":
+#          return string[:-2]
         
-      else:
-          return str(int(string[:2]) + 12) + string[2:8]
+#       else:
+#           return str(int(string[:2]) + 12) + string[2:8]
 
-#driver code
-time="01:58:42PM"
-print("12-hour Format time:: ", time)
-print("24-hour Format time ::",convert(time))
+# #driver code
+# time="01:58:42PM"
+# print("12-hour Format time:: ", time)
+# print("24-hour Format time ::",convert(time))
+
+
+
+
+
+
+# def findMedian(arr):
+#     # Write your code here
+#     counter = 0
+#     empty_list = []
+#     for i in range(arr):
+#         counter +=1
+#         empty_list.append(counter)
+#         midd_num = arr/2
+#         empty_list = empty_list[midd_num]
+#         print(empty_list)
+        
+# findMedian(7)        

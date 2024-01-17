5cb7331d5149f679cf50d9bf023b67b1b58eb480
unknown <mmohamedabdelm@gmail.com>
2024-01-17 02:24:27 +0200

    add qr-code automation

---
+++ automation     QR-code.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Make sure to pass the URL as a string
+url = 'https://github.com/Mohamed00Abdelmonem'
+
+# Create a QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("github-qr.png")

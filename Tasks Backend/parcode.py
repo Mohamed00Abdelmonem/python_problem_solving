fea9bdf5cf6ce7bf099756525bf4dd5c3bc1f8f3
unknown <mmohamedabdelm@gmail.com>
2024-01-17 02:22:26 +0200

    add code automation for qr-code urls

--- Tasks Backend/parcode.py
+++ Tasks Backend/parcode.py
@@ -1,11 +1,22 @@
-from barcode import EAN13
+import qrcode
 
-# Make sure to pass the number as string
-number = 'https://www.geeksforgeeks.org/how-to-generate-barcode-in-python/'
+# Make sure to pass the URL as a string
+url = 'https://github.com/Mohamed00Abdelmonem'
 
-# Now, let's create an object of EAN13
-# class and pass the number
-my_code = EAN13(number)
+# Create a QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
 
-# Our barcode is ready. Let's save it.
-my_code.save("new_code")
+# Add data to the QR code
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("github-qr.png")
